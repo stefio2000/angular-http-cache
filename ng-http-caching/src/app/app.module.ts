23eeee9cc50aa839d3f72f8f475d6091d874c976
stephanedavid <stephane.david.ext@aptar.com>
2022-10-20 12:47:42 +0200

    feat(ng-http-caching):full example

---
+++ ng-http-caching/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppComponent} from './app.component';
+import {NgHttpCachingModule, NgHttpCachingConfig, NgHttpCachingStrategy, NgHttpCachingLocalStorage, NgHttpCachingEntry} from 'ng-http-caching';
+import {HttpClientModule} from "@angular/common/http";
+
+// See https://github.com/nigrosimone/ng-http-caching#config
+const ngHttpCachingConfig: NgHttpCachingConfig = {
+  lifetime: 60000, // cache expire after 6000ms -> 60s
+  allowedMethod: ['GET', 'HEAD'],
+  cacheStrategy: NgHttpCachingStrategy.ALLOW_ALL,
+  isValid: (entry: NgHttpCachingEntry): boolean | undefined => {
+    // In this example only response with status code 200 can be stored into the cache
+    return entry.response.status === 200;
+  },
+};
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    NgHttpCachingModule.forRoot(ngHttpCachingConfig),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
