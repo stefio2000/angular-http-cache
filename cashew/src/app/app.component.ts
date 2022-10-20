a112446139541fff10ba091c0d6300a5fe4627de
stephanedavid <stephane.david.ext@aptar.com>
2022-10-20 14:25:35 +0200

    feat(cashew):full example

---
+++ cashew/src/app/app.component.ts
@@ -0,0 +1,42 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Subject, takeUntil} from "rxjs";
+import {withCache} from "@ngneat/cashew";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+})
+export class AppComponent implements OnInit, OnDestroy {
+  private static readonly SERVICE_EXAMPLE: string = 'https://jsonplaceholder.typicode.com/posts';
+  private destroy: Subject<void> = new Subject<void>();
+
+  title = '@ngneat/cashew';
+  data: { date: Date, data: any, cache: boolean }[] = [];
+
+  constructor(private readonly httpClient: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    // 1st call data will be cached
+    this.requestApi(false);
+
+    // 2nd call after 15s, data returned will be cached data
+    setTimeout(() => this.requestApi(true), 15000);
+
+    // 3rd call after 90s, data returned will be new data because cache should be expired
+    setTimeout(() => this.requestApi(false), 90000);
+  }
+
+  private requestApi(shouldBeCache: boolean): void {
+    this.httpClient.get(AppComponent.SERVICE_EXAMPLE, {
+      context: withCache()
+    })
+      .pipe(takeUntil(this.destroy))
+      .subscribe((data => this.data.push({date: new Date(), data, cache: shouldBeCache})));
+  }
+
+  ngOnDestroy(): void {
+    this.destroy.complete();
+  }
+}
---
+++ cashew/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+
+import {AppComponent} from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import {HttpCacheInterceptorModule} from '@ngneat/cashew';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    HttpCacheInterceptorModule.forRoot(
+      {
+        strategy: "explicit",
+        ttl: 60000,
+      }
+    )
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
